f4503429fcdbb70808f0fa1fe963e1260129dd6b
Oktawian Chojnacki <oktawian@me.com>
2016-05-29 08:52:18 +0200

    Fix formatting.

--- source/lsp.swift
+++ source/lsp.swift
@@ -5,7 +5,7 @@ Derived classes must be substitutable for their base classes.
 
 */
 
-let requestKey:NSString = "NSURLRequestKey"
+let requestKey: NSString = "NSURLRequestKey"
 
 // I'm a NSError subclass. I provide additional functionality but don't mess with original ones.
 class RequestError : NSError {
@@ -16,26 +16,26 @@ class RequestError : NSError {
 }
 
 // I fail to fetch data and will return RequestError.
-func fetchData(request:NSURLRequest) -> (data:NSData?, error:RequestError?) {
+func fetchData(request: NSURLRequest) -> (data: NSData?, error: RequestError?) {
 
-    let userInfo:[NSObject:AnyObject] = [ requestKey : request ]
+    let userInfo: [NSObject:AnyObject] = [ requestKey : request ]
 
-    return (nil, RequestError(domain:"DOMAIN", code: 1, userInfo: userInfo))
+    return (nil, RequestError(domain:"DOMAIN", code:0, userInfo: userInfo))
 }
 
 // I don't know what RequestError is and will fail and return a NSError.
-func willReturnObjectOrError() -> (object:AnyObject?, error:NSError?) {
+func willReturnObjectOrError() -> (object: AnyObject?, error: NSError?) {
 
     let request = NSURLRequest()
     let result = fetchData(request)
 
-    return (result.data , result.error)
+    return (result.data, result.error)
 }
 
 let result = willReturnObjectOrError()
 
 // Ok. This is a perfect NSError instance from my perspective.
-let error:Int? = result.error?.code
+let error: Int? = result.error?.code
 
 // But hey! What's that? It's also a RequestError! Nice!
 if let requestError = result.error as? RequestError {
