1f381a82cecdbe0286626c4a982e6d5886691be3
Oktawian Chojnacki <oktawian@me.com>
2019-04-15 14:55:35 +0300

    [Swift 5] Improve generator script

--- generate-playground.sh
+++ generate-playground.sh
@@ -4,12 +4,18 @@
 # Would love to rewrite it in Swift soon.
 
 combineSwift() {
-	cat source/startComment > $2
-	cat $1/header.md  >> $2
-	cat source/contents.md  >> $2
+	cat source/startComment >> $2
+	cat source/header.md >> $2
 	cat source/endComment >> $2
 	cat source/imports.swift >> $2
-	cat $1/*.swift >> $2
+	cat $1/srp.swift >> $2
+	cat $1/ocp.swift >> $2
+	cat $1/lsp.swift >> $2
+	cat $1/isp.swift >> $2
+	cat $1/dip.swift >> $2
+	cat source/startComment >> $2
+	cat source/footer.md  >> $2
+	cat source/endComment >> $2
 	{ rm $2 && awk '{gsub("\\*//\\*:", "", $0); print}' > $2; } < $2
 }
 
@@ -23,29 +29,25 @@ playground() {
 }
 
 combineMarkdown() {
-	cat $1/header.md  > $2
-
-	{ rm $2 && awk '{gsub("\\*/", "", $0); print}' > $2; } < $2
-	{ rm $2 && awk '{gsub("/\\*:", "", $0); print}' > $2; } < $2
-
+	cat source/header.md >> $2
 	cat source/startSwiftCode >> $2
-	cat $1/*.swift >> $2
+	cat $1/srp.swift >> $2
+	cat $1/ocp.swift >> $2
+	cat $1/lsp.swift >> $2
+	cat $1/isp.swift >> $2
+	cat $1/dip.swift >> $2
 
 	{ rm $2 && awk '{gsub("\\*//\\*:", "", $0); print}' > $2; } < $2
 	{ rm $2 && awk '{gsub("\\*/", "\n```swift", $0); print}' > $2; } < $2
 	{ rm $2 && awk '{gsub("/\\*:", "```\n", $0); print}' > $2; } < $2
+	{ rm $2 && awk '{gsub("```swift```", "", $0); print}' > $2; } < $2
 	
 	cat source/endSwiftCode >> $2
-
-	{ rm $2 && awk '{gsub("```swift```", "", $0); print}' > $2; } < $2
-
-	cat $2 >> README.md
-	rm $2
 }
 
 readme() {
 	echo "" > $2
-	combineMarkdown source/$1 $1.md
+	combineMarkdown source/$1 $2
 	cat source/footer.md  >> $2
 }
 
